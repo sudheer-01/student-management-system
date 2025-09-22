194804be6ac0f7b5caf591170354daa72d711273
sudheer-01 <sudheeradepu1@gmail.com>
2025-09-22 20:05:43 +0530

    into solution

--- homepageForFaculty/ViewMarks/viewMarks.js
+++ homepageForFaculty/ViewMarks/viewMarks.js
@@ -13,7 +13,8 @@ function fetchStudentData() {
         alert("Please select an exam.");
         return;
     }
-       
+       const selectedYear = localStorage.getItem("selectedYear");
+    const selectedBranch = localStorage.getItem("selectedBranch");
         const selectedSubject = localStorage.getItem("selectedSubject");
     fetch(`/getStudentMarks?exam=${selectedExam}&year=${selectedYear}&branch=${selectedBranch}&subject=${selectedSubject}`).then(response => response.json()).then(data => {
             tbody.innerHTML = ""; // Clear existing rows
