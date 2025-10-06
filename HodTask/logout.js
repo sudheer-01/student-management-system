4c8a19df266df0c5ddadf7425a9cfef535637b6d
sudheer-01 <sudheeradepu1@gmail.com>
2025-10-06 14:37:18 +0530

    adding the logout

---
+++ HodTask/logout.js
@@ -0,0 +1,9 @@
+const logoutBtn = document.getElementById("logoutBtn");
+if (logoutBtn) {
+        logoutBtn.addEventListener("click", function () {
+            if (!confirm("Log out?")) return;
+            fetch("/logout", { method: "POST" })
+                .then(() => { window.location.href = "/"; })
+                .catch(() => { window.location.href = "/"; });
+        });
+    }
