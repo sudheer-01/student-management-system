17d36a492f5c4ca24b51770302110f08a57b7b10
sudheer-01 <sudheeradepu1@gmail.com>
2026-01-03 21:21:14 +0530

    logout

--- verifyLogin.js
+++ verifyLogin.js
@@ -42,6 +42,8 @@ app.use(express.static(path.join(baseDir,"admin","resetPassword")));
 //forgot password
 app.use(express.static(path.join(baseDir,"ForgotPassword")));
 app.use(express.static(path.join(baseDir,"ForgotPassword","resetSTFPassword")));
+//Authentication
+app.use(express.static(path.join(baseDir,"Authentication")));
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
